6b8740307b1b2ab8b9eba50e6b974e3cf8aad52c
Ross Parkin <ross.parkin@armakuni.com>
2022-08-31 17:06:43 +0100

    Trained model

--- training/train.py
+++ training/train.py
@@ -1,19 +1,20 @@
+import json
+import os
 import pickle
 
 import pandas as pd
 
 from mlwrap import runners
 from mlwrap.enums import AlgorithmType, DataType, FeatureType
-from mlwrap.config import InputData, MLConfig, Feature, TrainingResults
+from mlwrap.config import InputData, MLConfig, Feature
 
 
 # read in the data
-df = pd.read_csv("../data/cleaned.csv")
+df = pd.read_csv("data/cleaned.csv")
 
 # define the features
 features = { 
         "Year" : Feature(id = "Year", feature_type=FeatureType.Continuous),
-        "State" : Feature(id = "State", feature_type=FeatureType.Categorical),
         "Total" : Feature(id = "Total", feature_type=FeatureType.Continuous)
     }
 
@@ -29,7 +30,18 @@ config = MLConfig(
 results = runners.train(config)
 
 # output the metrics
+scores = { k.name: float(v) for k,v in results.scores.items() }
+with open("metrics.json", "w") as f:
+    json.dump(scores, f)
 
 # save the model and encoders
-pickle.dump(results.model_bytes, open("../models/model.pkl", "wb"))
-pickle.dump(results.encoder_bytes, open("../models/encoder.pkl", "wb"))
+def save_pkl(path, obj):
+    dirname = os.path.dirname(path)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    with open(path, "wb") as f:
+        pickle.dump(obj, f)
+
+save_pkl("models/model.pkl", results.model_bytes)
+save_pkl("models/encoder.pkl", results.encoder_bytes)
