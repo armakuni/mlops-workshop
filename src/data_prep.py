c703cda578365e69b565b361a1330a7d563d30c8
Ross Parkin <ross.parkin@armakuni.com>
2022-11-11 09:13:37 +0000

    Add data_prep stub

---
+++ src/data_prep.py
@@ -0,0 +1,12 @@
+import os
+
+import pandas as pd
+
+input_path = "data/raw/Historical Station Counts by State 2007-2021.xlsx"
+
+# read in excel file
+
+# clean up data
+
+# Save the csv
+output_path = "data/processed/cleaned.csv"
